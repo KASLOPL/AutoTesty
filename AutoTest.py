4b9a4e2735332cad0819eb6aafe3a17a26aeb340
Kaslo <kasjan.bojko.gdansk@gmail.com>
2025-01-23 17:09:19 +0100

    fix

--- AutoTest.py
+++ AutoTest.py
@@ -1,5 +1,4 @@
 from selenium import webdriver
-from selenium.webdriver import Keys
 from selenium.webdriver.common.by import By
 from time import sleep
 from imap_tools import MailBox
@@ -17,12 +16,12 @@ driver.find_element(By.NAME, "email").send_keys(data("email"))
 driver.find_element(By.ID, "password").send_keys(data("account_password"))
 sleep(1.5)
 driver.find_element(By.CLASS_NAME, "neo-button.btn-primary.btn-large.neo-flex.justify-content-center.align-items-center.neo-box").click()
-sleep(2)
+sleep(3)
 with MailBox('imap.fastmail.com').login(data("email"), data("imap_password"), "Inbox") as mb:
     for msg in mb.fetch(limit=1, reverse=True, mark_seen=True):
-        dwafa = msg.html[10683:10689]
+        dwafa = msg.html[10683:10690]
 
 driver.find_element(By.ID, "verificationCode").send_keys(dwafa)
-driver.find_element(By.CLASS_NAME, "neo-button btn-primary btn-large neo-flex justify-content-center align-items-center neo-box").click()
+driver.find_element(By.XPATH, "//submit[1]").click()
 
 sleep(1000)
